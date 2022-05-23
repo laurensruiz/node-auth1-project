6bb2123b7676a43110a7edaeb5a0611ddcc68bc2
laurensruiz <laurenruiz97@yahoo.com>
2022-05-23 12:35:36 -0400

    set up router

--- api/server.js
+++ api/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const usersRouter = require('./users/users-router')
 
+const authRouter = require('./auth/auth-router')
 /**
+ * 
+ * //npm i express-session
   Do what needs to be done to support sessions with the `express-session` package!
   To respect users' privacy, do NOT send them a cookie unless they log in.
   This is achieved by setting 'saveUninitialized' to false, and by not
@@ -20,6 +24,9 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use('/api/users', usersRouter)
+server.use('/api/auth', authRouter)
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
